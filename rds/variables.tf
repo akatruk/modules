3259fc0a4db82819961fd0c29c3824fd3dd4b574
Andrei Katruk <a.katruk@live.com>
2022-05-04 16:14:12 +0300

    ff

--- rds/variables.tf
+++ rds/variables.tf
@@ -34,3 +34,22 @@ variable "tags" {
     }
   ]
 }
+
+# variable "tags" {
+#   type = list(object({
+#     project   = string
+#     owner     = string
+#     app_owner = string
+#     app-tasks = string
+#   }))
+#   default = [
+#     {
+#       project   = "test-project"
+#     owner     = "test-owner"
+#     app_owner = "test-app_owner"
+#     app-tasks = "test-app-tasks"
+#     }
+#   ]
+# }
+
+variable "tags" {}
