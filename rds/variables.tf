ff2896ebf8f1d1630ed397204f306f0017e08761
Andrei Katruk <a.katruk@live.com>
2022-05-04 16:15:19 +0300

    fff

--- rds/variables.tf
+++ rds/variables.tf
@@ -18,23 +18,6 @@ variable "replicas_count" {
   default = "1"
 }
 
-variable "tags" {
-  type = list(object({
-    project   = string
-    owner     = string
-    app_owner = string
-    app-tasks = string
-  }))
-  default = [
-    {
-      project   = "test-project"
-    owner     = "test-owner"
-    app_owner = "test-app_owner"
-    app-tasks = "test-app-tasks"
-    }
-  ]
-}
-
 # variable "tags" {
 #   type = list(object({
 #     project   = string
